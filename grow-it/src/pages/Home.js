d136e03b8ae8d09986284fe9051ff41337659b66
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-09-07 10:24:16 -0500

    scaled down to do dummy test. FE route hit

--- grow-it/src/pages/Home.js
+++ grow-it/src/pages/Home.js
@@ -1,25 +1,26 @@
-import { axios } from 'axios'
-import { useEffect, UseState } from 'react'
-import { BASE_URL } from '../globals'
+// import { axios } from 'axios'
+// import { useEffect, UseState } from 'react'
+// import { BASE_URL } from '../globals'
 
 const Home = (props) => {
-  const [plant, setPlant] = UseState('')
+  // const [plant, setPlant] = UseState('')
 
-  useEffect(() => {
-    const getPlants = async () => {
-      let res = await axios.get(`${BASE_URL}plants/`)
-      setPlant(res.data)
-    }
-    getPlants()
-  }, [props.recipe])
+  // useEffect(() => {
+  //   const getPlants = async () => {
+  //     let res = await axios.get(`${BASE_URL}plants/`)
+  //     setPlant(res.data)
+  //   }
+  //   getPlants()
+  // }, [props.recipe])
   return (
     <div>
-      {plant.map((plant) => (
+      {/* {plant.map((plant) => (
         <div>
           <img src={plant.image} alt={plant.name} />
           <h3>{plant.name}</h3>
         </div>
-      ))}
+      ))} */}
+      <h1>Werk!</h1>
     </div>
   )
 }
