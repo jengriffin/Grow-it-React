102dc37f49a046cf018d0d85fa29a82e88dba607
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-09-08 11:10:28 -0500

    input date being wack yo

--- grow-it/src/components/AddBlog.jsx
+++ grow-it/src/components/AddBlog.jsx
@@ -1,8 +1,8 @@
 import { BASE_URL } from '../globals'
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+
 
 const NewBlog =(props)=>{
   let navigate=useNavigate()
@@ -13,7 +13,7 @@ const NewBlog =(props)=>{
     body: ''
   }
   const [formState, setFormState] = useState(initialState)
-  console.log(`${BASE_URL}/blogs/`)
+
   useEffect(()=>{
     const getBlogs= async()=>{
       try{
@@ -24,5 +24,43 @@ const NewBlog =(props)=>{
     }
     getBlogs()
   },[])
+  const handleChange = (event) => {
+    console.log(event)
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    let res = await axios.post(`${BASE_URL}/blogs/`, formState)
+    setFormState(initialState)
+    navigate('/home')
+  }
+return(
+  <div className="form">
+      <form onSubmit={handleSubmit}>
+        <h1>New Note</h1>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          onChange={handleChange}
+          value={formState.title}
+        />
+        <label htmlFor="date">Date:</label>
+        <input type="date" 
+          onChange={handleChange}
+          value={formState.date}
+          // placeholder= "YYYY/MM/DD"
+        />
+        <label htmlFor="body">Type Here:</label>
+        <textarea
+          id="body"
+          onChange={handleChange}
+          value={formState.body}
+        /> <div>
+        <button type="submit">Submit</button>
+      </div>
+        </form>
+  </div>
+)
 }
 export default NewBlog
