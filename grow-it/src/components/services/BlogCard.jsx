b62a694910f5a07e23474b3e5d8cb8a9111d8ce2
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-09-10 14:16:55 -0500

    update form displaying

--- grow-it/src/components/services/BlogCard.jsx
+++ grow-it/src/components/services/BlogCard.jsx
@@ -1,77 +1,81 @@
-// import { BASE_URL } from '../globals'
-// import { useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom'
+import { BASE_URL } from '../../globals'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import csrftoken from '../../csrftoken'
+import { useNavigate } from 'react-router-dom'
 const BlogCard =()=>{
-//   const[blog, setBlog]=useState([])
+  const[blog, setBlog]=useState([])
+  let navigate= useNavigate()
 
-//   const initialState = {
-//     title:'',
-//     body: ''
-//   }
-//   const [formState, setFormState] = useState(initialState)
+  const initialState = {
+    title:'',
+    body: ''
+  }
+  const [formState, setFormState] = useState(initialState)
 
-//   let { id } = useParams()
+  let { id } = useParams()
 
-//   useEffect(() => {
-//     const selectedBlog = async () => {
-//       try {
-//         let res = await axios.get(`${BASE_URL}/blogs/${id}`)
-//         setBlog(res.data)
-//       } catch (eer) {
-//       }
-//     }
-//     setBlog(selectedBlog)
-//   }, [])
+  useEffect(() => {
+    const selectedBlog = async () => {
+      try {
+        let res = await axios.get(`${BASE_URL}/blogs/${id}`)
+        setBlog(res.data)
+      } catch (eer) {
+      }
+    }
+    setBlog(selectedBlog)
+  }, [])
 
-//   const handleChange = (event) => {
-//     console.log(event)
-//     setFormState({ ...formState, [event.target.id]: event.target.value })
-//   }
+  const handleChange = (event) => {
+    console.log(event)
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
 
-//   const handleSubmit = async (event) => {
-//     event.preventDefault()
-//     console.log(formState)
-//     console.log(csrftoken)
-//     await axios({
-//       url:`${BASE_URL}blogs/`,
-//       method:'POST',
-//       data: formState,
-//       headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json',
-//         'X-CSRFToken': csrftoken,
-//         'X-Requested-With': 'XMLHttpRequest', 
-//       },
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    console.log(formState)
+    console.log(csrftoken)
+    await axios({
+      url:`${BASE_URL}blogs/`,
+      method:'POST',
+      data: formState,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken,
+        'X-Requested-With': 'XMLHttpRequest', 
+      },
       
-//     })
-//     setFormState(initialState)
-//     navigate('/')
-//   }
-//   const [plant, setPlant] = useState([])
+    })
+    setFormState(initialState)
+    navigate('/')
+  }
+  const [plant, setPlant] = useState([])
 
 
-// return(
-//   <div className="form">
-//   <form onSubmit={handleSubmit}>
-//   <h1>New Note</h1>
-//   <label htmlFor="title">Title:</label>
-//   <input
-//     type="text"
-//     id="title"
-//     onChange={handleChange}
-//     value={formState.title}
-//   />
-//   <label htmlFor="body">Type Here:</label>
-//   <textarea
-//     id="body"
-//     onChange={handleChange}
-//     value={formState.body}
-//   /> <div>
-//   <button type="submit">Submit</button>
-//   </div>
-//   </form>
-//   </div>
-// )
+return(
+  <div className="form">
+  <form onSubmit={handleSubmit}>
+  <h1>Update Note</h1>
+  <label htmlFor="title">Title:</label>
+  <input
+    type="text"
+    id="title"
+    onChange={handleChange}
+    value={formState.title}
+  />
+  <label htmlFor="body">Type Here:</label>
+  <textarea
+    id="body"
+    onChange={handleChange}
+    value={formState.body}
+  /> <div>
+  <button type="submit">Submit</button>
+  </div>
+  </form>
+  </div>
+)
 
 
 
--- grow-it/src/globals.js
+++ grow-it/src/globals.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://django-test-mreep.herokuapp.com/'
+const BASE_URL = 'https://grow-it-back.herokuapp.com/'
 
 //  'http://127.0.0.1:8000/'
-
+//'https://django-test-mreep.herokuapp.com/'
 module.exports = {
   BASE_URL
 }
