b6f9db249e24c46fe1a10863f0d50a85095ecc77
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-09-11 18:39:41 -0500

    add cute buttons to home buttons refuse to navigate. Aggressive coding now in progress

--- grow-it/src/pages/Home.js
+++ grow-it/src/pages/Home.js
@@ -1,29 +1,31 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { BASE_URL } from '../globals'
+import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-  const [plant, setPlant] = useState([])
-
-  console.log(`${BASE_URL}plants/`)
-  useEffect(() => {
-    const getPlants = async () => {
-      let res = await axios.get(`${BASE_URL}plants/`)
-
-      console.log(res)
-      setPlant(res.data)
-    }
-    getPlants()
-  }, [])
+  const navigate = useNavigate()
   return (
     <div>
-      {plant.map((plant) => (
-        <div key={plant.id}>
-          <img src={plant.image} alt={plant.name} />
-          <h3>{plant.name}</h3>
-        </div>
-      ))}
-      <h1>Werk!</h1>
+      <button className="bg-green-100 rounded-lg justify-self-center h-full w-1/2">
+        <img
+          src="https://img.icons8.com/stickers/344/natural-food.png"
+          alt="a cheery little set of leaves"
+          className="object-scale-down h-20 mx-auto"
+        />
+      </button>
+
+      <button
+        className="bg-green-100 rounded-lg justify-self-center h-full w-1/2"
+        onClick="console.log(click)"
+      >
+        <img
+          src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/344/external-notebook-press-and-media-flaticons-lineal-color-flat-icons.png"
+          alt="a small terracotta spiral note book sitting closed under an orange pen waiting to be used"
+          className="object-scale-down h-20 mx-auto"
+        />
+      </button>
     </div>
   )
 }
