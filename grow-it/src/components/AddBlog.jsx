b484d28f7d018ac95986418b33e84617ff994484
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-09-11 17:02:20 -0500

    styling to form

--- grow-it/src/components/AddBlog.jsx
+++ grow-it/src/components/AddBlog.jsx
@@ -54,26 +54,34 @@ const NewBlog =(props)=>{
 console.log(`${BASE_URL}plants/`)
 
 return(
-  <div className="form">
-      <form onSubmit={handleSubmit}>
-        <h1>New Note</h1>
-        <label htmlFor="title">Title:</label>
-        <input
-          type="text"
-          id="title"
-          onChange={handleChange}
-          value={formState.title}
-        />
-        <label htmlFor="body">Type Here:</label>
-        <textarea
-          id="body"
-          onChange={handleChange}
-          value={formState.body}
-        /> <div>
-        <button type="submit">Submit</button>
-      </div>
+    <div className="form">
+        <form onSubmit={handleSubmit}>
+          <div class="grid gap-1 grid-cols-1 grid-rows-3">
+            <div class="grid justify-items-stretch">
+              <input
+                type="text"
+                id="title"
+                onChange={handleChange}
+                value={formState.title}
+                class="h-10 w-1/3 rounded-lg justify-self-center self-center text-3xl placeholder:italic mt-1 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
+                placeholder="Enter a title..."
+              />
+            </div>
+            <div class="grid justify-items-stretch">
+            <textarea
+              id="body"
+              onChange={handleChange}
+              value={formState.body}
+              class="h-40 w-1/2 rounded-lg justify-self-center text-2xl placeholder:italic focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
+              placeholder="Write a note..."
+            />
+            </div>
+            <div class="grid justify-items-stretch">
+              <button type="submit" class="bg-green-100 rounded-lg justify-self-center h-full w-1/2"><img src="https://www.freeiconspng.com/uploads/checkmark-png-5.png" class="object-scale-down h-20 mx-auto" /></button>
+            </div>
+          </div>
         </form>
-  </div>
-)
+    </div>
+  )
 }
 export default NewBlog
