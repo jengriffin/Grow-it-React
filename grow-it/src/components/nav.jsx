8dfb350ad0f514a63ff142a4e326d334b0aec449
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-09-11 16:15:17 -0500

    beginnings of the nav bar styling

--- grow-it/src/components/nav.jsx
+++ grow-it/src/components/nav.jsx
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom'
 const Nav = ()=>{
   return(
     <div>
-    <NavLink className='nav_links' to='/'>Home</NavLink>
-    <NavLink className='nav_links' to='/blog'>Add A Note</NavLink>
-    <NavLink className='nav_links' to='/blogs'>Notes</NavLink>
-    <NavLink to ='/plants'>Plants</NavLink>
+    <NavLink className="text-3xl text-gray-700 font-bold mb-5" to='/'>Home</NavLink>
+    <NavLink className="text-3xl text-gray-700 font-bold mb-5" to='/blog'>Add A Note</NavLink>
+    <NavLink className="text-3xl text-gray-700 font-bold mb-5" to='/blogs'>Notes</NavLink>
+    <NavLink className="text-3xl text-gray-700 font-bold mb-5" to ='/plants'>Plants</NavLink>
     </div>
   )
 }
