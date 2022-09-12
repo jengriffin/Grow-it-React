feafb96acae7dba62faacb45d0b6ad42908a50a9
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-09-11 19:43:43 -0500

    spooky online guides to try to get file uploads pray for us

--- grow-it/src/components/AddPlant.jsx
+++ grow-it/src/components/AddPlant.jsx
@@ -6,108 +6,89 @@ import csrftoken from '../csrftoken'
 
 
 
-const NewPlant=(props)=>{
-//   let navigate=useNavigate()
-//   const[newplant, setNewPlant]=useState([])
-//   const initialState = {
-//     name:'',
-//     image: null,
-//     info: ''
-//   }
-//   const [formState, setFormState] = useState(initialState)
+const NewPlant = (props) => {
+  let navigate = useNavigate()
 
-//   useEffect(()=>{
-//     const getPlants= async()=>{
-//       try{
-//         let res= await axios.get(`${BASE_URL}plants`)
-//         console.log(res.data)
-//         setNewPlant(res.data)
-//       }catch(eer){}
-//     }
-//     getPlants()
-//   },[])
-//   const handleChange = (event) => {
-//     console.log(event)
-//     setFormState({ ...formState, [event.target.id]: event.target.value })
-//   }
+  const [newplant, setNewPlant] = useState([])
+  const [uploadFile, setUploadFile] = useState("");
 
-//   const handleSubmit = async (event) => {
-//     event.preventDefault()
-//     console.log(formState)
-//     console.log(csrftoken)
-//     await axios({
-//       url:`${BASE_URL}plants/`,
-//       method:'POST',
-//       data: formState,
-//       headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json',
-//         'X-CSRFToken': csrftoken,
-//         'X-Requested-With': 'XMLHttpRequest', 
-//       },
-      
-//     })
-//     setFormState(initialState)
-//     navigate('/')
-//   }
-  
+  const initialState = {
+    name:'',
+    image: '',
+    info: ''
+  }
 
-// console.log(`${BASE_URL}plants/`)
-// onFileChange = event => {
-//   this.setState({ image: event.target.files[0] });
-// }
-// onFileUpload = () => {
-    
-//   // Create an object of formData
-//   const formData = new FormData();
+  const [formState, setFormState] = useState(initialState)
 
-//   // Update the formData object
-//   formData.append(
-//     "myFile",
-//     this.state.image,
-//     this.state.image.name
-//   );
+  useEffect(()=>{
+    const getPlants= async()=>{
+      try{
+        let res= await axios.get(`${BASE_URL}plants`)
+        console.log(res.data)
+        setNewPlant(res.data)
+      }catch(eer){}
+    }
+    getPlants()
+  },[])
 
-//   // Details of the uploaded file
-//   console.log(this.state.image);
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
 
-//   // Request made to the backend api
-//   // Send formData object
-//   axios.post("api/uploadfile", formData);
-// };
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const form_data = new FormData();
+    form_data.append('image', uploadFile);
+    form_data.append('name', formState.name);
+    form_data.append('info', formState.info);
+
+    await axios({
+      url:`${BASE_URL}plants/`,
+      method:'POST',
+      data: form_data,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'X-CSRFToken': csrftoken,
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+
+    })
+    setFormState(initialState)
+    navigate('/')
+  }
 
 
-// return(
-//   <div className="form">
-//       <form onSubmit={handleSubmit}>
-//         <h1>Add a Plant!</h1>
-//         <label htmlFor="name">Name:</label>
-//         <input
-//           type="text"
-//           id="name"
-//           onChange={handleChange}
-//           value={formState.name}
-//         />
-//         <label htmlFor="image">Picture:</label>
-//         <input
-//           type="file"
-//           id="image"
-//           onChange={this.onFileChange}
-//           value={formState.image}
-//         />
-//         <button onClick={this.onFileUpload}>
-//                   Upload!
-//                 </button>
-//         <label htmlFor="info">Info:</label>
-//         <textarea
-//           id="info"
-//           onChange={handleChange}
-//           value={formState.info}
-//         /> <div>
-//         <button type="submit"><img src="https://www.freeiconspng.com/uploads/checkmark-png-5.png" /></button>
-//       </div>
-//         </form>
-//   </div>
-// )
+
+
+console.log(`${BASE_URL}plants/`)
+
+return(
+  <div className="form">
+      <form onSubmit={handleSubmit}>
+        <h1>Add a Plant!</h1>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          onChange={handleChange}
+          value={formState.name}
+        />
+        <label htmlFor="image">Picture:</label>
+
+        <input type="file" onChange={(e) => setUploadFile(e.target.files[0])} />
+
+        <label htmlFor="info">Info:</label>
+        <textarea
+          id="info"
+          onChange={handleChange}
+          value={formState.info}
+        />
+        <div>
+          <button type="submit"><img src="https://www.freeiconspng.com/uploads/checkmark-png-5.png" /></button>
+        </div>
+      </form>
+  </div>
+)
 }
 export default NewPlant
