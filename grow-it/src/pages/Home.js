622ef125c65ce2499f9569a78c742ddfc2967e71
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-09-12 10:39:58 -0500

    pictures on home now navigate, bonus they aligned themselves the way I wanted without me doing anything

--- grow-it/src/pages/Home.js
+++ grow-it/src/pages/Home.js
@@ -2,23 +2,19 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { BASE_URL } from '../globals'
 import { NavLink } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Routes, Route } from 'react-router-dom'
+import Blogs from './Blogs'
 
 const Home = () => {
-  const navigate = useNavigate()
-  return (
-    <div>
-      <button className="bg-green-100 rounded-lg justify-self-center h-full w-1/2">
-        <img
-          src="https://img.icons8.com/stickers/344/natural-food.png"
-          alt="a cheery little set of leaves"
-          className="object-scale-down h-20 mx-auto"
-        />
-      </button>
-
+  const navigate = useNavigate
+  const notes = () => {
+    navigate('/blogs')
+  }
+  const navBlog = () => {
+    return (
       <button
+        onClick={() => navigate('/blogs')}
         className="bg-green-100 rounded-lg justify-self-center h-full w-1/2"
-        onClick="console.log(click)"
       >
         <img
           src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/344/external-notebook-press-and-media-flaticons-lineal-color-flat-icons.png"
@@ -26,6 +22,30 @@ const Home = () => {
           className="object-scale-down h-20 mx-auto"
         />
       </button>
+    )
+  }
+  return (
+    <div>
+      <NavLink
+        className="bg-green-100 rounded-lg justify-self-center h-full w-1/2"
+        to="/plants"
+      >
+        <img
+          src="https://img.icons8.com/stickers/344/natural-food.png"
+          alt="a cheery little set of leaves"
+          className="object-scale-down h-20 mx-auto"
+        />
+      </NavLink>
+      <NavLink
+        className="bg-green-100 rounded-lg justify-self-center h-full w-1/2"
+        to="/blogs"
+      >
+        <img
+          src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/344/external-notebook-press-and-media-flaticons-lineal-color-flat-icons.png"
+          alt="a small terracotta spiral note book sitting closed under an orange pen waiting to be used"
+          className="object-scale-down h-20 mx-auto"
+        />
+      </NavLink>
     </div>
   )
 }
